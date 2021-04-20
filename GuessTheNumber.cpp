389be3084cda44e172cc3be6c268cf2d355e5d4e
Radostina <radi.chipanova@gmail.com>
2021-04-20 18:59:00 +0300

    Fixed spacing

--- GuessTheNumber.cpp
+++ GuessTheNumber.cpp
@@ -2,28 +2,30 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
- 
+
 int main()
 {
-	srand(time(0)); //seed random number generator
-	int num = rand() % 100 + 1; // random number between 1 and 100
-	cout << "Guess My Number Game\n\n";
-  
-  int guess = 0;
-  while (guess != num){
-		cout << "Enter a guess between 1 and 100 : ";
-		cin >> guess;
+    srand(time(0)); //seed random number generator
+    int num = rand() % 100 + 1; // random number between 1 and 100
+    cout << "Guess My Number Game\n\n";
 
-		if (guess > num) {
-			cout << "Too high!\n\n";
-    }
-		else if (guess < num) {
-			cout << "Too low!\n\n";
-    }
-		else {
-			cout << "\nCorrect! You got it in ";
-    }
-	} 
+    int guess = 0;
+    while (guess != num) {
+        cout << "Enter a guess between 1 and 100 : ";
+        cin >> guess;
 
-	return 0;
+        if (guess > num) {
+            cout << "Too high!\n\n";
+        }
+        else if (guess < num)
+            a
+            {
+                cout << "Too low!\n\n";
+            }
+        else {
+            cout << "\nCorrect! You got it in ";
+        }
+    }
+
+    return 0;
 }
